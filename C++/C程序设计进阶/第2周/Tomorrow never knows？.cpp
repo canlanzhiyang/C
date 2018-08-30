d943dd1d8575ae11995865108923bd4ba1a9f923
canlanzhiyang <canlanzhiyang@gmail.com>
2018-08-31 06:49:30 +0800

    真不容易

--- C++/C程序设计进阶/第2周/Tomorrow never knows？.cpp
+++ C++/C程序设计进阶/第2周/Tomorrow never knows？.cpp
@@ -9,7 +9,7 @@ int main( )
 	int y , m , d;
 	bool ly;
 	cin >> y >> c >> m >> c >> d;
-	if ( ( y % 4 == 0 ) && ( y % 400 != 0 ) )
+	if ( (( y % 4 == 0 ) && ( y % 100!=0))|| ( y % 400 == 0 ) )
 	{
 		switch ( m )
 		{
